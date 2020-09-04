bee5f0584c4c17c06b1129d7a02cdab82f7cc2f7
Brandon Roffis <broffis@outlook.com>
2020-09-04 17:58:36 -0400

    Added sidebar nav with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
+
+import axios from './modules/axios';
 
 import './assets/style/all.scss';
 
@@ -7,27 +9,53 @@ import Roster from './containers/Roster';
 import Group from './components/GroupScoreboard';
 import Groups from './containers/Groups';
 import FullScoreboard from './containers/Scoreboard';
+import SidebarMenu from './components/SideBarMenu';
 
 function App() {
+  const isMountedRef = useRef(null);
+
   const competitionId = "5f481e84691a987f93f9bfb0";
+  const [competition, setCompetition] = useState();
+  const [showSideBarMenu, toggleShowSideBarMenu] = useState(false);
+  const [groups, setGroups] = useState();
+
+  useEffect(() => {
+    isMountedRef.current = true;
+
+    if (isMountedRef) {
+      axios.get(`/groups/filter/byComp/${competitionId}`)
+        .then(groups => setGroups(groups.data))
+
+      axios.get(`/comps/${competitionId}`)
+        .then(comps => setCompetition(comps.data))
+    }
+    
+    return () => isMountedRef.current = false;
+  }, [competitionId]);
+
+  
+
+  let competitionName = '';
+
+  if (competition && competition.name) {
+    competitionName = competition.name
+  }
+
   return (
     <div className="App">
       <nav className="nav-bar">
         <ul className="nav">
-          <li className="nav__link">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav__link">
-            <Link to="/roster">Roster</Link>
-          </li>
-          <li className="nav__link">
-            <Link to="/groups">Groups</Link>
-          </li>
-          <li className="nav__link">
-            <Link to="/scoreboard">Scoreboard</Link>
+          <div className="hamburger-container" onClick={() => toggleShowSideBarMenu(!showSideBarMenu)} >
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
+          <li className="nav__link nav__link--text">
+            <Link to="/scoreboard">{competitionName}</Link>
           </li>
         </ul>
       </nav>
+      <SidebarMenu competition={competition} groups={groups} showMenu={showSideBarMenu} close={() => toggleShowSideBarMenu(!showSideBarMenu)}/>
       <Switch>
         <Route path="/scoreboard/" component={() => <FullScoreboard competition_id={competitionId}/>}/>
         <Route path="/roster/" component={Roster} />
---
+++ src/components/SideBarMenu.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Backdrop from './Backdrop';
+
+const SideBarMenu = (props) => {
+  let comp_name, groups_dropdown;
+
+  if (props.groups && props.groups.length >= 1) {
+    console.log('we have groups');
+
+    groups_dropdown = props.groups.map(group => {
+      return (
+        <li className="side-bar-menu__sub-link">
+          <Link to={`/group/${group._id}`}>{group.group_name}</Link>
+        </li>
+      )
+    })
+  }
+
+  if (props.competition && props.competition.name) {
+    comp_name = props.competition.name;
+  }
+
+  return (
+    <React.Fragment>
+      <Backdrop show={props.showMenu} clicked={props.close} />
+      <div className={`side-bar-menu ${ props.showMenu ? 'isVisible' : 'hidden'}`} onClick={props.close}>
+        <span className="side-bar-menu__close-button" onClick={props.close}><img src={`${window.location.origin}/icons/close.svg`} alt=""/></span>
+        <p className="side-bar-menu__competition-name">
+          <Link to="/">{comp_name}</Link>
+        </p>
+        <ul className="side-bar-menu__links">
+          <li className="side-bar-menu__link">
+            <Link to="/scoreboard">Scoreboard</Link>
+          </li>
+          <li className="side-bar-menu__link">
+            <Link to="/scoreboard">Seeding - Update Link</Link>
+          </li>
+          <li className="side-bar-menu__link">
+            <Link to="/roster">Roster</Link>
+          </li>
+          <li className="side-bar-menu__link">
+            <Link to="/scoreboard">Bracket - Update Link</Link>
+          </li>
+          <li className="side-bar-menu__link side-bar-menu__sub-links-btn">
+            <Link to="/groups">Groups</Link>
+            <ul className="side-bar-menu__sub-links">
+              {
+                groups_dropdown
+              }
+            </ul>
+          </li>
+            
+        </ul>
+      </div>
+    </React.Fragment>
+    
+  ); 
+}
+
+export default SideBarMenu;
